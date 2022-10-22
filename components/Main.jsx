67de7208cc5d5d18a0f021c6f4beb3417cde4699
Samuel Hawkins <Samuel.hawkins1998@gmail.com>
2022-10-21 20:22:25 -0400

    feat

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
   return (
     <div className= 'w-full h-screen text-center'>
         <div className= 'max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
             <div>
-                <p className='uppercase text-sm tracking-widest text-gray-600'> LET'S BUILD SOMETHING TOGETHER</p>
+                
+                <p className='uppercase text-sm tracking-widest text-gray-600'> LET'S BUILD SOMETHING TOGETHER
+                </p>
+                <h1 className='py-4 text-gray-700'>
+                    Hi, I'm<span className= 'text-[#5651e5]'> Samuel 
+
+                </span>
+                </h1>
+                <h1 className='py-2 text-gray-700'>
+                    A Full Stack Developer 
+                </h1>
+                <p className='py-4 text-gray-600 max-w-[70%]m-auto'>
+                    I'm a full stack developer specializing in 
+                    building exceptional digital experiences. 
+                    Currently, I'm focused on building responsive 
+                    front-end web applications while learning back-end 
+                    technologies. 
+                </p>
+                <div className = 'flex item-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className = 'rounded-full shadow-lg shadow-gray-400 p-6 hover:scale-110 ease-in duration-400'>
+                        <FaLinkedinIn /> 
+                    </div>
+                    <div className = 'rounded-full shadow-lg shadow-gray-400 p-6 hover:scale-110 ease-in duration-400'>
+                        <FaGithub /> 
+                    </div>
+                    <div className = 'rounded-full shadow-lg shadow-gray-400 p-6 hover:scale-110 ease-in duration-400'>
+                        <AiOutlineMail /> 
+                    </div>
+                    <div className = 'rounded-full shadow-lg shadow-gray-400 p-6 hover:scale-110 ease-in duration-400'>
+                        <BsFillPersonLinesFill /> 
+                    </div>
+
+
+                </div>
             </div>
         </div>
     </div>
